aa919a0afe5aca099954f6a428ea0caffac3340c
Βάγγο <evangelos.poneres@gmail.com>
2023-12-19 00:16:22 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Pontian Greek Society "Xeniteas" of Chicago, IL
+    pontiangreeks.org
+## Notes
+- Due to losing the original hosting provider, all site content was lost
+- The content in the repository's initial commit was extracted from the [Wayback Machine](web.archive.org)
---
+++ wp-login.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Pontian Greek Society of Chicago &lsaquo; Log In</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<link rel='stylesheet' href='http://pontiangreeks.org/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.6.1' type='text/css' media='all' />
+<meta name='robots' content='noindex,follow' />
+<link rel="icon" href="http://pontiangreeks.org/wp-content/uploads/2016/10/ficon.png" sizes="32x32" />
+<link rel="icon" href="http://pontiangreeks.org/wp-content/uploads/2016/10/ficon.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="http://pontiangreeks.org/wp-content/uploads/2016/10/ficon.png" />
+<meta name="msapplication-TileImage" content="http://pontiangreeks.org/wp-content/uploads/2016/10/ficon.png" />
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us">
+		<div id="login">
+		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Pontian Greek Society of Chicago</a></h1>
+	
+<form name="loginform" id="loginform" action="http://pontiangreeks.org/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+		<input type="hidden" name="redirect_to" value="http://pontiangreeks.org/wp-admin/" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="http://pontiangreeks.org/wp-login.php?action=lostpassword">Lost your password?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="http://pontiangreeks.org/">&larr; Back to Pontian Greek Society of Chicago</a></p>
+	
+	</div>
+
+	
+		<div class="clear"></div>
+	</body>
+	</html>
+	
