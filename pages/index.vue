850d9c99d573da3de24962267456572128416e8f
lipsotiko <evangelos.poneres@gmail.com>
2024-12-22 18:25:14 -0600

    Add Vasilopita Cutting Event

--- pages/index.vue
+++ pages/index.vue
@@ -6,106 +6,36 @@
       <ILayout>
         <IRow no-gutter>
           <IColumn md="12" lg="4">
+          </IColumn>
+          <IColumn md="12" lg="4">
             <div class="news-item card">
               <div>
                 <a
-                  href="https://pontiangreeksocietyofchicago.ticketspice.com/47th-annual-dinner-dance"
+                  target="_blank"
+                  href="/2025_Vassilopita_Event.png"
                 >
                   <img
-                    src="~/assets/images/2024_dinner_dance_thumbnail.jpg"
-                    alt="2024 dinner dance"
+                    src="~/assets/images/2025_Vassilopita_Event_thumbnail.jpg"
+                    alt="Jan. 5th, 2025"
                   />
                 </a>
               </div>
               <div>
                 <h6>
                   <a
-                    href="https://pontiangreeksocietyofchicago.ticketspice.com/47th-annual-dinner-dance"
+                    target="_blank"
+                    href="/2025_Vassilopita_Event.png"
                   >
-                    47th Annual Dinner Dance
+                    Vasilopita Cutting
                   </a>
                 </h6>
                 <p>
-                  Saturday, November 9th, 2024 | 7:00pm<br />
-                  <br />
-                  Nikos Banquets <br />
-                  7600 S. Harlem Ave. <br />
-                  Bridgeview, IL
+                  Saturday, January 5th, 2025 | 3:00pm
                 </p>
-                <div class="card-bottom">
-                  <a
-                    class="wx-button"
-                    href="https://pontiangreeksocietyofchicago.ticketspice.com/47th-annual-dinner-dance"
-                    style="
-                      background: #c8a15e;
-                      color: white;
-                      padding: 10px 20px;
-                      text-decoration: none;
-                      font-weight: bold;
-                    "
-                    target="_blank"
-                    >Get Tickets</a
-                  >
-                </div>
               </div>
             </div>
           </IColumn>
           <IColumn md="12" lg="4">
-            <div class="news-item card">
-              <div>
-                <div>
-                  <a
-                    href="/home/the-genocide-of-greeks-of-the-asia-minor-1913-1923/"
-                  >
-                    <img
-                      src="~/assets/images/2016/12/painting.jpg"
-                      alt="painting"
-                    />
-                  </a>
-                </div>
-                <div>
-                  <h6>
-                    <a
-                      href="/home/the-genocide-of-greeks-of-the-asia-minor-1913-1923/"
-                    >
-                      The Genocide of Greeks of the Asia Minor: 1913 - 1923
-                    </a>
-                  </h6>
-                  <p>
-                    Thursday, March 9, 2017 | 9:00am-2:30pm<br /><br />
-                    Please join us on March 9th, 2017 at the Illinois Holocaust
-                    Museum & Education Center for a special teacher's
-                    workshop...<a
-                      href="/home/the-genocide-of-greeks-of-the-asia-minor-1913-1923/"
-                      >More detail</a
-                    >
-                  </p>
-                </div>
-              </div>
-            </div>
-          </IColumn>
-          <IColumn md="12" lg="4">
-            <div class="news-item card">
-              <div>
-                <a href="/home/new-publications/">
-                  <img
-                    src="~/assets/images/2016/12/New-Publications.jpg"
-                    alt="new-publications"
-                  />
-                </a>
-              </div>
-              <h6>
-                <a href="/home/new-publications/">New Publications</a>
-              </h6>
-              <p>
-                We are pleased to see two more new publications that add to the
-                effort of publishing historical information and facts in English
-                on the uprooting, destruction and genocide of Hellenism of
-                Anatolia. Both books are available to be purchased through
-                Amazon. Kostas Faltaits -The Genocide of the Greeks in Turkey:
-                Survivor Testimonies From The Nikomedia [&hellip;]
-              </p>
-            </div>
           </IColumn>
         </IRow>
       </ILayout>
