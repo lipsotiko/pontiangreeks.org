1e231d9369a1e23bf4d5205ca74fb7e67e16e296
lipsotiko <evangelos.poneres@gmail.com>
2025-11-21 10:48:06 -0600

    Christmas party 2025

--- pages/index.vue
+++ pages/index.vue
@@ -2,27 +2,30 @@
   <IContainer class="no-padding">
     <ILayout>
       <IRow no-gutter>
-        <!--<IColumn md="12" lg="3">
+        <IColumn v-if="isEventPast('2025-12-14')" md="12" lg="3">
           <aside class="left-aside">
             <div class="news-item card">
               <div>
-                <a target="_blank" href="/2025_Vassilopita_Event.png">
-                  <img src="~/assets/images/2025_Vassilopita_Event_thumbnail.jpg" alt="Jan. 5th, 2025" />
+                <a target="_blank" href="/Pontian_Society_Christmas_Party_2025.pdf">
+                  <img src="~/assets/images/2025_Christmas_Party_thumbnail.jpg" alt="Jan. 5th, 2025" />
                 </a>
               </div>
               <div>
                 <h6>
-                  <a target="_blank" href="/2025_Vassilopita_Event.png">
-                    Vasilopita Cutting
+                  <a target="_blank" href="/Pontian_Society_Christmas_Party_2025.pdf">
+                    Christmas Party
                   </a>
                 </h6>
                 <p>
-                  Saturday, January 5th, 2025 | 3:00pm
+                  Saturday, December 13th, 2025 | 6:30pm<br>
+                  Click here to <a target="_blank" href="https://docs.google.com/forms/d/e/1FAIpQLScVvrPBHFCmAXIW6Gb8XPOkfK3StLJbVrMxMdqfzQg_JAd-xg/viewform?pli=1">
+                    RSVP
+                  </a>
                 </p>
               </div>
             </div>
           </aside>
-        </IColumn>-->
+        </IColumn>
         <IColumn sm="12">
           <PhotoCarousel />
         </IColumn>
@@ -30,6 +33,9 @@
     </ILayout>
   </IContainer>
 </template>
+<script setup>
+const isEventPast = (targetDate) => new Date() < new Date(targetDate);
+</script>
 <style scoped>
 .no-padding {
   padding: 0;;
@@ -42,5 +48,9 @@
 .news-item img {
   width: 100%;
   margin-bottom: 8px;
+}
+
+h6, p {
+  margin: 0 2px;
 }
 </style>
